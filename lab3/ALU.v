36521e40b06b8e2bab2afc66bfe0bfbe17dd817f
github desktop <gwangjin@postech.ac.kr>
2019-03-18 01:47:38 +0900

    fix define variable

--- lab3/ALU.v
+++ lab3/ALU.v
@@ -47,32 +47,32 @@ module ALU (A, B, FuncCode, C, OverflowFlag);
 	always @(A or B or FuncCode) begin
 		OverflowFlag = 0;
 		case (FuncCode)
-			ADD: begin
+			`ADD: begin
 				C = A + B;
 				//set overflow flag
 				if  ((A[`NumBits-1] ^ C[`NumBits-1]) & (B[`NumBits-1] ^ C[`NumBits-1]))
 				OverflowFlag = 1;
 			end
-			SUB: begin
+			`SUB: begin
 				C = A - B;
 				//set underflow flag
 				if ((A[`NumBits-1] ^ C[`NumBits-1]) & (B[`NumBits-1] ^~ C[`NumBits-1]))
 				OverflowFlag = 1;
 			end
-			ID: C = A;
-			NOT: C = ~A;
-			AND: C = A&B ;
-			OR: C = A|B ;
-			NAND: C = ~(A&B);
-			NOR: C = ~(A|B);
-			XOR: C = A^B;
-			XNOR: C = A^~B;
-			LLS: C = A<<1;
-			LRS: C = A>>1;
-			ALS: C = A<<1;
-			ARS: C = {A[`NumBits-1], A[`NumBits-1:1]};
-			TCP: C = ~A + 1;
-			ZERO: C = 16'h0000;
+			`ID: C = A;
+			`NOT: C = ~A;
+			`AND: C = A&B ;
+			`OR: C = A|B ;
+			`NAND: C = ~(A&B);
+			`NOR: C = ~(A|B);
+			`XOR: C = A^B;
+			`XNOR: C = A^~B;
+			`LLS: C = A<<1;
+			`LRS: C = A>>1;
+			`ALS: C = A<<1;
+			`ARS: C = {A[`NumBits-1], A[`NumBits-1:1]};
+			`TCP: C = ~A + 1;
+			`ZERO: C = 16'h0000;
 			default: C = 16'h0000;
 		endcase
 	end
--- lab3/cpu.v
+++ lab3/cpu.v
@@ -29,58 +29,58 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	  if (opcode == 15) begin // R-Type
 	    if (func == 25) begin // JPR
 			controls[11:5] <= 7'b1000000;
-			controls[4:1] <= ZERO;
+			controls[4:1] <= `ZERO;
 			controls[0] <= 1'b0;
 		else if (func == 26) begin // JRL
 			controls[11:5] <= 7'b1010001;
-			controls[4:1] <= ZERO;
+			controls[4:1] <= `ZERO;
 			controls[0] <= 1'b0;
 		else begin
 			controls[11:5] <= 7'b0000011;
 			case (func) 
-				INST_FUNC_ADD : controls[4:1] <= ADD; 
-				INST_FUNC_SUB : controls[4:1] <= SUB;
-				INST_FUNC_AND : controls[4:1] <= AND;
-				INST_FUNC_ORR : controls[4:1] <= OR;
-				INST_FUNC_NOT : controls[4:1] <= NOT;
-				INST_FUNC_TCP : controls[4:1] <= TCP;
-				INST_FUNC_SHL : controls[4:1] <= ALS;
-				INST_FUNC_SHR : controls[4:1] <= ARS;
-				default : controls[4:1] <= ZERO;
+				INST_FUNC_ADD : controls[4:1] <= `ADD; 
+				INST_FUNC_SUB : controls[4:1] <= `SUB;
+				INST_FUNC_AND : controls[4:1] <= `AND;
+				INST_FUNC_ORR : controls[4:1] <= `OR;
+				INST_FUNC_NOT : controls[4:1] <= `NOT;
+				INST_FUNC_TCP : controls[4:1] <= `TCP;
+				INST_FUNC_SHL : controls[4:1] <= `ALS;
+				INST_FUNC_SHR : controls[4:1] <= `ARS;
+				default : controls[4:1] <= `ZERO;
 			endcase
 			controls[0] <= 1'b0;
 		end
 	  end else if (opcode == 4'd4) begin
 	    controls[11:5] <= 7'b0000001;
-		controls[4:1] <= ADD;
+		controls[4:1] <= `ADD;
 		controls[0] <= 1'b1;
 	  end else if (opcode == 4'd5) begin
 	    controls[11:5] <= 7'b0000001;
-		controls[4:1] <= OR;
+		controls[4:1] <= `OR;
 		controls[0] <= 1'b1;
 	  end else if (opcode == 4'd6) begin
 	    controls[11:5] <= 7'b0010001
-		controls[4:1] <= ZERO;
+		controls[4:1] <= `ZERO;
 		controls[0] <= 1'b0;
 	  end else if (opcode == 4'd7) begin
 	    controls[11:5] <= 7'b0011001;
-		controls[4:1] <= ADD;
+		controls[4:1] <= `ADD;
 		controls[0] <= 1'b1;
 	  end else if (opcode == 4'd8) begin
 	    controls[11:5] <= 7'b0000100;
-		controls[4:1] <= ADD;
+		controls[4:1] <= `ADD;
 		controls[0] <= 1'b1;
 	  end else if (opcode >= 4'd0 && opcode <= 4'd3) begin
 		controls[11:5] <= 7'b0100000;
-		controls[4:1] <= ADD;
+		controls[4:1] <= `ADD;
 		controls[0] <= 1'b1;
 	  end else if (opcode == 9)
 	    controls[11:5] <= 7'b1000000;
-		controls[4:1] <= ZERO;
+		controls[4:1] <= `ZERO;
 		controls[0] <= 1'b1;
 	  end else if (opcode == 10)
 	    controls[11:5] <= 7'b1010001;
-		controls[4:1] <= ZERO;
+		controls[4:1] <= `ZERO;
 		controls[0] <= 1'b1;
 	  end else begin
 		controls[11:0] <= 12'd0;
