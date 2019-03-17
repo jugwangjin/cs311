9852378775b0029c19daff34334f564e6d1b6b0a
github desktop <gwangjin@postech.ac.kr>
2019-03-18 02:21:12 +0900

    finding syntax errors

--- lab3/cpu.v
+++ lab3/cpu.v
@@ -12,7 +12,7 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	input reset_n;									
 	input clk;	
 
-	reg [`Word_Size:0]instruction; 
+	wire [`WORD_SIZE:0]instruction; 
 
 	reg [11:0]controls; // 11 Jump, 10 Branch, 9 MemtoReg, 8 MemRead, 7 MemWrite, 6 RegDst, 5 RegWrite, 4:1 [3:0]ALUOp, 0 ALUSrc;
 
@@ -31,25 +31,24 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 			controls[11:5] <= 7'b1000000;
 			controls[4:1] <= `ZERO;
 			controls[0] <= 1'b0;
-		else if (func == 26) begin // JRL
+		end else if (func == 26) begin // JRL
 			controls[11:5] <= 7'b1010001;
 			controls[4:1] <= `ZERO;
 			controls[0] <= 1'b0;
-		else begin
+		end else begin
 			controls[11:5] <= 7'b0000011;
 			case (func) 
-				INST_FUNC_ADD : controls[4:1] <= `ADD; 
-				INST_FUNC_SUB : controls[4:1] <= `SUB;
-				INST_FUNC_AND : controls[4:1] <= `AND;
-				INST_FUNC_ORR : controls[4:1] <= `OR;
-				INST_FUNC_NOT : controls[4:1] <= `NOT;
-				INST_FUNC_TCP : controls[4:1] <= `TCP;
-				INST_FUNC_SHL : controls[4:1] <= `ALS;
-				INST_FUNC_SHR : controls[4:1] <= `ARS;
+				`INST_FUNC_ADD : controls[4:1] <= `ADD; 
+				`INST_FUNC_SUB : controls[4:1] <= `SUB;
+				`INST_FUNC_AND : controls[4:1] <= `AND;
+				`INST_FUNC_ORR : controls[4:1] <= `OR;
+				`INST_FUNC_NOT : controls[4:1] <= `NOT;
+				`INST_FUNC_TCP : controls[4:1] <= `TCP;
+				`INST_FUNC_SHL : controls[4:1] <= `ALS;
+				`INST_FUNC_SHR : controls[4:1] <= `ARS;
 				default : controls[4:1] <= `ZERO;
 			endcase
 			controls[0] <= 1'b0;
-		end
 	  end else if (opcode == 4'd4) begin
 	    controls[11:5] <= 7'b0000001;
 		controls[4:1] <= `ADD;
@@ -59,7 +58,7 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 		controls[4:1] <= `OR;
 		controls[0] <= 1'b1;
 	  end else if (opcode == 4'd6) begin
-	    controls[11:5] <= 7'b0010001
+	    controls[11:5] <= 7'b0010001;
 		controls[4:1] <= `ZERO;
 		controls[0] <= 1'b0;
 	  end else if (opcode == 4'd7) begin
@@ -74,11 +73,11 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 		controls[11:5] <= 7'b0100000;
 		controls[4:1] <= `ADD;
 		controls[0] <= 1'b1;
-	  end else if (opcode == 9)
+	  end else if (opcode == 9) begin
 	    controls[11:5] <= 7'b1000000;
 		controls[4:1] <= `ZERO;
 		controls[0] <= 1'b1;
-	  end else if (opcode == 10)
+	  end else if (opcode == 10) begin
 	    controls[11:5] <= 7'b1010001;
 		controls[4:1] <= `ZERO;
 		controls[0] <= 1'b1;
