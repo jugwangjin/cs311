9f067f745feb34df4c3eed15ce54f532db200071
github desktop <gwangjin@postech.ac.kr>
2019-04-22 20:43:16 +0900

    retry always taken

--- lab5_always_taken/datapath.v
+++ lab5_always_taken/datapath.v
@@ -296,14 +296,7 @@ module datapath (Clk, Reset_N, readM1, address1, data1, readM2, writeM2, address
 
             // IFID Latch
             IFID_instruction = instruction;
-            if (ID_stall == 1'b0) begin
-                if(IF_flush) begin
-                    IFID_IsBubble = 1'b1;
-                end
-                else begin
-                    IFID_IsBubble = 1'b0;
-                end
-            end
+            IFID_IsBubble = ID_stall || IF_flush;
         end
     end
 
