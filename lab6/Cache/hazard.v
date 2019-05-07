c488cf68037d029adb7843ca0c77bf5b280955ed
github desktop <gwangjin@postech.ac.kr>
2019-05-07 13:47:06 +0900

    debug cache

--- lab6/Cache/hazard.v
+++ lab6/Cache/hazard.v
@@ -1,13 +1,15 @@
 `include "opcodes.v"
 
-module hazard(stall, IDEX_IsBubble, ID_IsJumpR, use_rs, rs, use_rt, rt, IDEX_MemRead, IDEX_rd);
+module hazard(stall, M1busy, IFID_IsBubble, ID_IsJumpI, use_rs, rs, use_rt, rt, IDEX_MemRead, IDEX_rd);
     output stall;
     wire stall;
 
-    input IDEX_IsBubble;
-    wire IDEX_IsBubble;
-    input ID_IsJumpR;
-    wire ID_IsJumpR;
+    input M1busy;
+    wire M1busy;
+    input IFID_IsBubble;
+    wire IFID_IsBubble;
+    input ID_IsJumpI;
+    wire ID_IsJumpI;
     input use_rs;
     input [1:0]rs;
     input use_rt;
@@ -26,6 +28,6 @@ module hazard(stall, IDEX_IsBubble, ID_IsJumpR, use_rs, rs, use_rt, rt, IDEX_Mem
     assign rsstall = (rs == IDEX_rd) && use_rs == 1'b1;
     assign rtstall = (rt == IDEX_rd) && use_rt == 1'b1;
 
-    assign stall = (((rsstall == 1'b1 || rtstall == 1'b1) && IDEX_MemRead == 1'b1) || (IDEX_IsBubble == 1'b0 && ID_IsJumpR == 1'b1)) ? 1'b1 : 1'b0;
+    assign stall = (((rsstall == 1'b1 || rtstall == 1'b1) && IDEX_MemRead == 1'b1) || (M1busy == 1'b1 && IFID_IsBubble == 1'b0 && ID_IsJumpI == 1'b1)) ? 1'b1 : 1'b0;
 
 endmodule
